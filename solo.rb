65ca944743635084e0e2054f0f52cd3fdec15d89
bis5 <bis5@bis5.mydns.jp>
2013-06-07 16:36:04 +0900

    add proxy conf on solo.rb

--- solo.rb
+++ solo.rb
@@ -4,3 +4,5 @@ encrypted_data_bag_secret "/tmp/chef-solo/data_bag_key"
 cookbook_path             [ "/tmp/chef-solo/site-cookbooks",
                             "/tmp/chef-solo/cookbooks" ]
 role_path                 "/tmp/chef-solo/roles"
+http_proxy                "http://202.26.158.1:8080"
+https_proxy               "https://202.26.158.1:8080"
